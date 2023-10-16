91ac6b0d90b4c3909abeb33f0036cb9908778dac
Téo Froissart <teofroissart@mbp-de-teo.home>
2023-10-17 00:08:52 +0200

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import WebContainer from './components/WebContainer.jsx'
 import { Route, Routes } from "react-router-dom";
-import PresentationPage from './components/BottomSection/PresentationPage';
+// import PresentationPage from './components/BottomSection/PresentationPage';
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
 	<div className="App">
 		<Routes>
 			<Route path="/" element={<WebContainer/>}/>
-			<Route path="/presentation" element={<PresentationPage/>}/>
+			{/* <Route path="/presentation" element={<PresentationPage/>}/> */}
 		</Routes>
     </div>
   );
