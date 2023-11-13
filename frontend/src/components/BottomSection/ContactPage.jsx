cc9b41ba395be50cfd10ed2b46b4ca69afe527c5
Téo Froissart <teofroissart@mbp-de-teo.home>
2023-11-13 18:00:05 +0100

    debut contact

--- frontend/src/components/BottomSection/ContactPage.jsx
+++ frontend/src/components/BottomSection/ContactPage.jsx
@@ -1,2 +1,20 @@
 import React from 'react';
+import { Box, TextField } from '@mui/material';
 
+const ContactPage = () => {
+	return (
+		<Box className="App__WebContainer__BottomSection__ContactPage__Main">
+			Contactez moi :
+			<TextField id="outlined-basic" label="Full Name" variant="outlined" margin="dense" className="App__WebContainer__BottomSection__ContactPage__Main__Name">
+			</TextField>
+			<TextField id="outlined-basic" label="Company" variant="outlined" margin="dense" className="App__WebContainer__BottomSection__ContactPage__Main__Name">
+			</TextField>
+			<TextField id="outlined-basic" label="E-Mail" variant="outlined" margin="dense" className="App__WebContainer__BottomSection__ContactPage__Main__Name">
+			</TextField>
+			<TextField id="outlined-basic" label="Message" multiline maxRows={4} margin="dense" className="App__WebContainer__BottomSection__ContactPage__Main__Name">
+			</TextField>
+		</Box>
+	);
+}
+
+export default ContactPage;
